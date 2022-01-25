7aea394f18957a331b2662b6a669b91ce89df042
Stefan Behrendt <stefan.behrendt@onexip.com>
2022-01-25 19:50:18 +0100

    added new logo; minor fixes

--- apps/frontend-new/src/app/core/store/home/home.effects.ts
+++ apps/frontend-new/src/app/core/store/home/home.effects.ts
@@ -3,27 +3,28 @@ import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { of } from 'rxjs';
 import { catchError, exhaustMap, map } from 'rxjs/operators';
-import { AppState } from 'src/app/interfaces/app-state.interface';
+import { AppState } from '../../../app.state.interface';
 import * as HomeActions from './home.actions';
 
 @Injectable()
 export class HomeEffects {
   constructor(
     private store: Store<AppState>,
-    private actions$: Actions,
-    private homeService: HomeService
-  ) {}
+    private actions$: Actions
+  ) // private homeService: HomeService
+  {}
 
-  getMessage$ = createEffect(() =>
-    this.actions$.pipe(
-      ofType(HomeActions.getMessage),
-      exhaustMap(() =>
-        this.homeService.getMessage$().pipe(
-          map((message: string) =>
-            FlightsActions.getMessageSuccess({ message })
-          ),
-          catchError(() => of(HomeActions.getMessageFailed()))
-        )
-      )
-    )
-  );
+  // getMessage$ = createEffect(() =>
+  //   this.actions$.pipe(
+  //     ofType(HomeActions.getMessage),
+  //     exhaustMap(() =>
+  //       this.homeService.getMessage$().pipe(
+  //         map((message: string) =>
+  //           FlightsActions.getMessageSuccess({ message })
+  //         ),
+  //         catchError(() => of(HomeActions.getMessageFailed()))
+  //       )
+  //     )
+  //   )
+  // );
+}
